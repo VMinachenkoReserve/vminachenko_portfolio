4a810968b891ddebca3fb04a6f003f7ec447cedb
VMinachenko <87294451+VMinachenko@users.noreply.github.com>
2021-07-31 20:57:16 +0300

    Update loader.js

--- src/components/icons/loader.js
+++ src/components/icons/loader.js
@@ -4,23 +4,7 @@ const IconLoader = () => (
   <svg id="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
     <title>Loader Logo</title>
     <g>
-      <g id="B" transform="translate(11.000000, 5.000000)">
-      <text
-      transform="translate(293 324)"
-      fill="#63ffda"
-      font-size="200"
-      font-family="SegoeUI-Semibold, Segoe UI"
-      font-weight="600"
-      >
-      <tspan x="13" y="0">
-        V
-      </tspan>
-    </text>
-        <path
-          d="M435.651,850.174l126.064-68.158V637.388L437.036,563.135,310.695,632.124V776.474Z"
-          fill="currentColor"
-        />
-      </g>
+      <g id="B" transform="translate(11.000000, 5.000000)"></g>
       <path
         stroke="currentColor"
         strokeWidth="5"
@@ -34,6 +18,9 @@ const IconLoader = () => (
                   L 89, 28 z"
       />
     </g>
+    <text x="35" y="70" fill="currentColor" fontSize="55px" fontFamily="Consolas">
+      V
+    </text>
   </svg>
 );
 
