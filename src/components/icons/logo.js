ca61656b1728752af62de624a2dd37a845aca0c2
VMinachenko <87294451+VMinachenko@users.noreply.github.com>
2021-07-31 19:40:41 +0300

    Update logo.js

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -4,7 +4,7 @@ const IconLogo = () => (
   <svg id="logo" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 84 96">
     <title>Logo</title>
     <g transform="translate(-8.000000, -2.000000)">_</g>
-    <text x="30" y="65" fill="currentColor" font-size="45px" font-family="Consolas">V</text> =$0
+    <text x="30" y="65" fill="currentColor" font-size="45px" font-family="Consolas">V</text>
    </svg>   
       <g transform="translate(11.000000, 5.000000)">
         <polygon
