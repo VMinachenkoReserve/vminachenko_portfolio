dc1ca6b399bb328b2cc570e6d77c05ee4af7a2d1
VMinachenko <87294451+VMinachenko@users.noreply.github.com>
2021-07-31 19:58:11 +0300

    Update logo.js

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -3,7 +3,7 @@ import React from 'react';
 const IconLogo = () => (
   <svg id="logo" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 84 96">
     <title>Logo</title>
-    <g transform="translate(-8.000000, -2.000000)">
+    <g transform="translate(-8.000000, -2.000000)">...</g>
     <text x="30" y="65" fill="currentColor" fontSize="50px" fontFamily="Consolas">
       V
     </text> 
@@ -17,7 +17,6 @@ const IconLogo = () => (
           points="39 0 0 22 0 67 39 90 78 68 78 23"
         />
       </g>
-    </g>
   </svg>
 );
 
