0ed82a1393222fa0b49492f205fb1982ae340d2c
VMinachenko <87294451+VMinachenko@users.noreply.github.com>
2021-07-31 20:33:32 +0300

    Update loader.js

--- src/components/icons/loader.js
+++ src/components/icons/loader.js
@@ -1,27 +1,33 @@
 import React from 'react';
 
 const IconLoader = () => (
-  <svg id="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-    <title>Loader Logo</title>
-    <g>
-      <g id="B" transform="translate(11.000000, 5.000000)"></g>
-      <path
-        stroke="currentColor"
-        strokeWidth="5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M 50, 5
-                  L 11, 27
-                  L 11, 72
-                  L 50, 95
-                  L 89, 73
-                  L 89, 28 z"
-      />
-    </g>
-    <text x="35" y="70" fill="currentColor" fontSize="55px" fontFamily="Consolas">
-      V
+  <svg
+  xmlns="http://www.w3.org/2000/svg"
+  width="261.021"
+  height="298.536"
+  viewBox="0 0 261.021 298.536"
+>
+  <g transform="translate(-238.695 -107.379)">
+    <text
+      transform="translate(293 324)"
+      fill="#63ffda"
+      font-size="200"
+      font-family="SegoeUI-Semibold, Segoe UI"
+      font-weight="600"
+    >
+      <tspan x="12" y="-5">
+        V
+      </tspan>
     </text>
-  </svg>
+    <path
+      d="M435.651,850.174l126.064-68.158V637.388L437.036,563.135,310.695,632.124V776.474Z"
+      transform="translate(-67 -450)"
+      fill="none"
+      stroke="#63ffda"
+      stroke-width="10"
+    />
+  </g>
+</svg>
 );
 
 export default IconLoader;
