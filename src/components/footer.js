e2c00b4d29e25c97ade464e17dad725aa5e666dc
VMinachenko <87294451+VMinachenko@users.noreply.github.com>
2021-08-01 07:10:32 +0300

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -107,8 +107,7 @@ const Footer = () => {
       <StyledCredit tabindex="-1">
         <a href="https://github.com/bchiang7/v4">
           <div>
-           Designed &amp; Built by Brittany Chiang<br></br>
-           Revised by Viacheslav Minachenko
+           Link to the creator<br></br>
           </div>
 
           {githubInfo.stars && githubInfo.forks && (
