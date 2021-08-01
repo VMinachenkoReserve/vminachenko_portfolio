b6df82d9a09d21ca169d33a0f65568bc7b9931e5
VMinachenko <87294451+VMinachenko@users.noreply.github.com>
2021-08-01 07:29:49 +0300

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -107,7 +107,7 @@ const Footer = () => {
       <StyledCredit tabindex="-1">
         <a href="https://github.com/bchiang7/v4">
           <div>
-           Link to the creator<br></br>
+           Link to the template creator<br></br>
           </div>
 
           {githubInfo.stars && githubInfo.forks && (
