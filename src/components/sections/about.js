c2a22d2897b2b4b58ea243ded7225e7184fd9296
VMinachenkoReserve <87294451+VMinachenkoReserve@users.noreply.github.com>
2024-08-27 12:38:52 +0500

    Update about.js

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -126,7 +126,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['Python (pandas, numpy, plotly, sklearn, pytorch)', 'Postgres, MS SQL, MySQL, MongoDB', 'Docker','GIT','ORM, SOAP/REST Systems, Kafka, Elasticsearch, Spark','AI-Model Chain Systems, Airflow, Tableau, Oracle BI','WSGI, uvicorn', 'SQL, YAML, Bash'];
+  const skills = ['Python (pandas, numpy, plotly, sklearn, pytorch)', 'Postgres, MS SQL, MySQL, MongoDB', 'Docker','GIT','ORM, SOAP/REST Systems, Spark','AI-Model Chain Systems, Airflow, Tableau, Oracle BI','SQL, YAML'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
